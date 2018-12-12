1b3737ac8ba28c56411a556925aa5d7c6cdfd200
Joe H <schosev@yahoo.com>
2018-12-12 11:14:15 -0600

    Updates

---
+++ precache-manifest.c54b6f7cc01f78304dbf30027a319575.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "12ba87071cbc7b5fd5a35d5ac5880a0c",
+    "url": "/profile-react/static/media/star-wars.12ba8707.jpg"
+  },
+  {
+    "revision": "21e72517174c52ebf388",
+    "url": "/profile-react/static/css/main.fa078abc.chunk.css"
+  },
+  {
+    "revision": "7c44167ca9efa703b988c52c90dc78e4",
+    "url": "/profile-react/static/media/co-view-4.7c44167c.jpg"
+  },
+  {
+    "revision": "512641571638c297c3aa",
+    "url": "/profile-react/static/js/1.51264157.chunk.js"
+  },
+  {
+    "revision": "5b840cd6f667d68ae5ba",
+    "url": "/profile-react/static/js/runtime~main.5b840cd6.js"
+  },
+  {
+    "revision": "eadf1069fd671fcf7678bbd5d29e9753",
+    "url": "/profile-react/static/media/Joe-Profile2.eadf1069.jpg"
+  },
+  {
+    "revision": "21e72517174c52ebf388",
+    "url": "/profile-react/static/js/main.21e72517.chunk.js"
+  },
+  {
+    "revision": "f4f74fd956dbd486eb935937fcca927d",
+    "url": "/profile-react/static/media/recycling.f4f74fd9.jpg"
+  },
+  {
+    "revision": "998fb06f55f1501140cddea7ed437310",
+    "url": "/profile-react/static/media/waterfall2.998fb06f.jpg"
+  },
+  {
+    "revision": "d615d6626850589507ee87abcc3afda4",
+    "url": "/profile-react/static/media/nyt.d615d662.jpg"
+  },
+  {
+    "revision": "9a1e03522892aa6adaa23795bc236bbc",
+    "url": "/profile-react/static/media/cat.9a1e0352.jpeg"
+  },
+  {
+    "revision": "512641571638c297c3aa",
+    "url": "/profile-react/static/css/1.c1d2872a.chunk.css"
+  },
+  {
+    "revision": "20bb5d45eaa4f69beda14e7898a0f754",
+    "url": "/profile-react/index.html"
+  }
+];
